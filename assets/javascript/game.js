ba4ce037ad9303b2017b33d991f4561e3ad02ae6
TDang <tin2dang@gmail.com>
2018-07-25 13:01:33 -0500

    updated JS to update wins and losses tracker

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,49 @@ var losses = 0;
 
 // Create an array that will hold the user's current guesses
 var currentGuess = [];
-// Create a variable for the guess to hold temporarily
-// var guess;
+var guessesLeft = 9;
+
+//Computer will pick out a random selection from the array
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+//
+
+function updateHTML() {
+    var html =
+        "<h1>The Psychic Game</h1>" +
+        "<p>Guess what letter I'm thinking of</p>" +
+        "<p>Wins:" + wins + "</p>" +
+        "<p>Losses:" + losses + "</p>" +
+        "<p>Guesses Left:" + guessesLeft + "</p>" +
+        "<p>Your Guesses so far:" + currentGuess + "</p>";
+
+    // // Change the HTML to reflect the updates
+    document.getElementById("game").innerHTML = html;
+}
+
+function outcome(result) {
+    
+    if (result === "win") {
+        wins++;
+    }
+
+    else if (result === "lose") {
+        losses++;
+    }
+
+    //Then reset the brackets
+    currentGuess = [];
+    guessesLeft = 9;
+
+    //Computer will pick out a new random selection from the array
+    computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+    updateHTML();
+}
 
 //This function activates when the user presses a key.
 document.onkeyup = function (event) {
 
-    //Computer will pick out a random selection from the array
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
     //Holds the value of what key was pressed
     var userGuess = event.key;
@@ -22,33 +57,44 @@ document.onkeyup = function (event) {
     //In the case the user chooses a capital letter
     var userGuess = userGuess.toLowerCase();
 
-    //This will add the guess to the array
-    currentGuess.push(userGuess);
+    //Computer's current choice
+    console.log("Computer's current guess is " + computerGuess);
 
 
     //To check if the js is reading the guesses correctly
+
     console.log(userGuess);
     //the score will increment wins score when the user guesses what the computer wants.
 
     // If the userguess is equal to the one of the computer choices
-    if (userGuess === d/*the array of computerChoice*/) {
+    if (computerChoice.includes(userGuess)/*the array of computerChoice*/) {
 
+        //This will add the guess to the array
+        currentGuess.push(userGuess);
+
+        // if user guesses correctly
         if (userGuess === computerGuess) {
-            wins++;
-        } else {
-            losses++;
+            outcome("win");
         }
+        // if they run out of guesses
+        else if (guessesLeft === 0) {
+            outcome("lose");
+        }
+        // while thye still play the game
+        else {
+            guessesLeft--
+        }
+        // updates html so they can see what they can guess
+        updateHTML();
 
-        //Create a variable to hold the html that will keep scores. 
-        var html =
-            "<h1>The Psychic Game</h1>"
-        "<p>Guess what letter I'm thinking of</p>"
-        "<p>Wins:" + wins + "</p>"
-        "<p>Losses:" + losses + "</p>"
-        "<p>Guesses Left:9</p>"
-        "<p>Your Guesses so far:</p>"
-        //Reset the guesses left to 9 when the game ends and wipe the section to the left of your guesses so far
+        // //Reset the guesses left to 9 when the game ends and wipe the section to the left of your guesses so far
+        // // if (currentGuess > 9){
+        //     /// wipe arrary
+        //     currentGuess = [];
     }
-
 }
+
+
+
+
 
